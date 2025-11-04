91e5a136305af4db038b06f2aed7c33aab201f7e
Zhao <hong.zhao@wur.nl>
2025-11-04 10:20:34 +0100

    Add initial test cases for daily_min()

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,8 +2,9 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
-from inflammation.models import daily_mean
+from inflammation.models import daily_mean, daily_min
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -28,4 +29,10 @@ def test_daily_mean_integers():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
 
